d9caa222b494153754c08b0241f55a1923dbf794
Omar-Belalami <omarbelalami@yahoo.com>
2023-07-07 00:14:51 +0200

    js e debut modal (css à faire)

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,44 +1,46 @@
+let userConnected = false;
+
 function getAllCredentials() {
     // Récupère l'email entré par l'utilisateur
     const email = document.getElementById('email').value;
-  
+
     // Récupère le mot de passe
     const password = document.getElementById('password').value;
-    
+
     // Effectue une requête POST vers le lien de l'Api
     fetch('http://localhost:5678/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ email: email, password: password }),
-  
-      // Définit le type de contenu de la requête comme JSON
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
-        // Affiche les données renvoyées par l'api dans la console
-        console.log(data);
-  
-        // Vérifie si un token est renvoyé dans la réponse
-        if (data.token) {
-          // Redirige l'utilisateur vers la page principale
-          window.location.href = 'index.html';
-  
-        } else {
-          // Alerte indiquant que les identifiants sont incorrects
-          alert('Oops! Identifiants incorrects. Veuillez réessayer.');
+        method: 'POST',
+        body: JSON.stringify({ email: email, password: password }),
+
+        // Définit le type de contenu de la requête comme JSON
+        headers: {
+            'Content-Type': 'application/json'
         }
-      })
-      .catch(error => {
-        // Affiche une erreur dans la console en cas de problème avec la requête
-        console.error('Oh non! Une erreur est survenue :', error);
-      });
-  }
-  
-  // Récupère le bouton de soumission du formulaire dans le document
-  const submitButton = document.querySelector('.form button');
-  
-  // écouteur d'événements pour détecter le clic sur le bouton de soumission
-  submitButton.addEventListener('click', getAllCredentials);
-  
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Affiche les données renvoyées par l'api dans la console
+            console.log(data);
+            // Vérifie si un token est renvoyé dans la réponse
+            if (data.token) {
+                localStorage.setItem('userToken', data.token);
+
+                // Redirige l'utilisateur vers la page principale
+                window.location.href = 'index.html';
+               
+            } else {
+                // Alerte indiquant que les identifiants sont incorrects
+                alert('Oops! Identifiants incorrects. Veuillez réessayer.');
+            }
+        })
+        .catch(error => {
+            // Affiche une erreur dans la console en cas de problème avec la requête
+            console.error('Oh non! Une erreur est survenue :', error);
+        });
+}
+
+// Récupère le bouton de soumission du formulaire dans le document
+const submitButton = document.querySelector('.form button');
+
+// écouteur d'événements pour détecter le clic sur le bouton de soumission
+submitButton.addEventListener('click', getAllCredentials);
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,64 +1,8 @@
 /*
 *
-On selection toutes les images des projets, et leurs data via l'API
-*
+On selection toutes les images des projets, et leurs data via l'Api
 */
-
-fetch('http://localhost:5678/api/works', {  // Récupère les données depuis l'API
-    headers: {  // Entêtes de la requête
-        'Accept': 'application/json'  // Accepte les réponses au format JSON
-    }
-})
-    .then(response => response.json())  // Convertit la réponse en JSON
-    .then(data => {
-        const données = data;  // Récupère les données
-        const galleryElement = document.querySelector('.gallery');  // Sélectionne l'élément de la galerie
-
-        données.forEach(donnée => {
-            const imageUrl = donnée.imageUrl;
-
-            // Crée un élément image
-            const imageElement = document.createElement('img');
-            imageElement.src = imageUrl;
-
-            // Ajoute l'image à la galerie
-            galleryElement.appendChild(imageElement);
-        });
-    })
-    .catch(error => {
-        console.error(error);
-    });
-
-
-/*
-*
-Ici on va créer les boutons et les filtres pour gerer les differents projets
-*
-*/
-const galleryDiv = document.querySelector('.gallery');  // Sélectionne le div de la galerie
-const buttonsDiv = document.querySelector('.filter-buttons');  // Sélectionne le div des boutons de filtre
-
-function createFilterButtons(categories) {
-    const showAllButton = document.createElement('button');
-    showAllButton.textContent = 'Tous';  // Texte du bouton "Tous"
-    showAllButton.addEventListener('click', function () {
-        filterImages(0);  // Affiche toutes les images (categoryId = 0)
-    });
-    buttonsDiv.appendChild(showAllButton);  // Ajoute le bouton "Tous" au div des boutons de filtre
-
-    categories.forEach(category => {
-        const button = document.createElement('button');
-        button.textContent = category.name;  // Texte du bouton de la catégorie
-
-        button.addEventListener('click', function () {
-            filterImages(category.id);  // Filtre les images en fonction de l'ID de la catégorie
-        });
-
-        buttonsDiv.appendChild(button);  // Ajoute le bouton au div des boutons de filtre
-    });
-}
-
-function filterImages(categoryId) {
+function galleryImages(categoryId) {
     fetch('http://localhost:5678/api/works', {
         headers: {  // Entêtes de la requête
             'Accept': 'application/json'  // Accepte les réponses au format JSON
@@ -67,21 +11,108 @@ function filterImages(categoryId) {
         .then(response => response.json())  // Convertit la réponse en JSON
         .then(data => {
             const images = data;  // Récupère les images depuis les données
-            const filteredImages = images.filter(image => image.categoryId === categoryId || categoryId === 0);  // Filtre les images en fonction de l'ID de la catégorie
-
+            const galleryFilteredImages = images.filter(image => image.categoryId === categoryId || categoryId === 0);  // Filtre les images en fonction de l'ID de la catégorie
+            const galleryDiv = document.querySelector('.gallery');
             galleryDiv.innerHTML = '';  // Vide la galerie
-
-            filteredImages.forEach(image => {
+            galleryFilteredImages.forEach(image => {
                 const img = document.createElement('img');
                 img.src = image.imageUrl;
                 img.alt = image.title;
-
                 galleryDiv.appendChild(img);  // Ajoute les images filtrées à la galerie
             });
         })
         .catch(error => {
             console.error(error);
         });
+
+}
+/** 
+ * Ici on va créer les boutons et les filtres pour gerer les differents projets
+*/
+
+function createFilterButtons(categories) {
+    const buttonsDiv = document.querySelector('.filter-buttons');
+    const showAllButton = document.createElement('button');
+    showAllButton.textContent = 'Tous';  // Texte du bouton "Tous"
+    showAllButton.addEventListener('click', function () {
+        galleryImages(0);  // Affiche toutes les images (categoryId = 0)
+    });
+    buttonsDiv.appendChild(showAllButton);  // Ajoute le bouton "Tous" au div des boutons de filtre
+
+    categories.forEach(category => {
+        const button = document.createElement('button');
+        button.textContent = category.name;  // Texte du bouton de la catégorie
+
+        button.addEventListener('click', function () {
+            galleryImages(category.id);  // Filtre les images en fonction de l'ID de la catégorie
+        });
+
+        buttonsDiv.appendChild(button);  // Ajoute le bouton au div des boutons de filtre
+    });
+}
+
+/**
+ * 
+ */
+
+//const iconDiv = document.querySelector('.icon');
+let userToken = localStorage.getItem('userToken');
+
+
+function setConnectedMode() {
+    const iconDiv = document.createElement('div');
+    iconDiv.classList.add('icon');
+    const iconElement = document.createElement('i');
+    iconElement.classList.add("fa-light");
+    iconElement.classList.add("fa-pen-to-square");
+    const divModal = document.createElement('div');
+    divModal.classList.add('modal');
+    divModal.id = 'modal';
+    iconDiv.appendChild(iconElement);
+    iconDiv.appendChild(divModal);
+    const sectionPortfolio = document.getElementById('portfolio');
+    sectionPortfolio.appendChild(iconDiv);
+    const galleryDiv = document.createElement('div');
+    galleryDiv.classList.add('gallery');
+    sectionPortfolio.appendChild(galleryDiv);
+
+    iconElement.addEventListener('click', function () {
+        const modal = document.createElement('div');
+        modal.classList.add('modal'); 
+        modal.id = 'modal';
+        document.body.appendChild(modal);
+
+        const galleryImages = document.querySelectorAll('.gallery img');
+
+        galleryImages.forEach(image => {
+            const modalImage = document.createElement('img');
+            modalImage.src = image.src;
+            modalImage.alt = image.alt;
+            modal.appendChild(modalImage);
+        });
+
+        modal.style.display = 'block';
+
+        window.addEventListener('click', function (event) {
+            const modal = document.getElementById('modal');
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
+
+    });
+
+}
+
+function setDisconnectedMode(categories) {
+    const sectionPortfolio = document.getElementById('portfolio');
+    const filterDiv = document.createElement('div');
+    filterDiv.classList.add('filter-buttons');
+    sectionPortfolio.appendChild(filterDiv);
+    const galleryDiv = document.createElement('div');
+    galleryDiv.classList.add('gallery');
+    sectionPortfolio.appendChild(galleryDiv);
+    createFilterButtons(categories);
 }
 
 fetch('http://localhost:5678/api/categories', {
@@ -92,13 +123,27 @@ fetch('http://localhost:5678/api/categories', {
     .then(response => response.json())  // Convertit la réponse en JSON
     .then(data => {
         const categories = data;  // Récupère les catégories depuis les données
+        if (userToken) {
+            console.log('token!!!!!!!!!!')
+            setConnectedMode();
 
-        createFilterButtons(categories);  // Crée les boutons de filtre en fonction des catégories
-
-        filterImages(0);  // Applique le filtre initial pour afficher toutes les images
+        }
+        else {
+            console.log('pas de token---------')
+            setDisconnectedMode(categories);
+            // Crée les boutons de filtre si l'utilisateur n'est pas connecté
+        }
+        galleryImages(0);  // Applique le filtre initial pour afficher toutes les images
     })
     .catch(error => {
         console.error(error);
     });
 
+
+
+
+
+/**
+ * <font-awesome-icon icon="fa-light fa-pen-to-square" />
+ */
 
