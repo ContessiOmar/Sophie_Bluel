36a8e13b02fc9a01626ee70500477727d33a0a60
Omar-Belalami <omarbelalami@yahoo.com>
2023-08-08 01:14:43 +0200

    login js implementation

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,3 +1,4 @@
+// Funzione per gestire l'invio del form
 function getAllCredentials() {
     // Récupère l'email entré par l'utilisateur
     const email = document.getElementById('email').value;
@@ -9,8 +10,6 @@ function getAllCredentials() {
     fetch('http://localhost:5678/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ email: email, password: password }),
-
-        // Définit le type de contenu de la requête comme JSON
         headers: {
             'Content-Type': 'application/json'
         }
@@ -22,10 +21,8 @@ function getAllCredentials() {
             // Vérifie si un token est renvoyé dans la réponse
             if (data.token) {
                 sessionStorage.setItem('userToken', data.token);
-
                 // Redirige l'utilisateur vers la page principale
                 window.location.href = 'index.html';
-               
             } else {
                 // Alerte indiquant que les identifiants sont incorrects
                 alert('Oops! Identifiants incorrects. Veuillez réessayer.');
@@ -33,12 +30,44 @@ function getAllCredentials() {
         })
         .catch(error => {
             // Affiche une erreur dans la console en cas de problème avec la requête
-            console.error('Oh non! Une erreur est survenue :', error);
+            alert('Oh non! Une erreur est survenue :', error);
         });
 }
 
-// Récupère le bouton de soumission du formulaire dans le document
+// Récupère les éléments du formulaire et du bouton de soumission
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
 const submitButton = document.querySelector('.form button');
 
-// écouteur d'événements pour détecter le clic sur le bouton de soumission
-submitButton.addEventListener('click', getAllCredentials);
+// Fonction pour vérifier la validité du formulaire
+function checkFormValidity() {
+    const emailValue = emailInput.value.trim();
+    const passwordValue = passwordInput.value.trim();
+
+    if (emailValue !== '' && passwordValue !== '') {
+        // Activer le bouton de soumission si les champs sont remplis
+        submitButton.removeAttribute('disabled');
+    } else {
+        // Désactiver le bouton de soumission si les champs ne sont pas remplis
+        submitButton.setAttribute('disabled', 'disabled');
+    }
+}
+
+// Écouteurs d'événements pour vérifier la validité du formulaire lors de la saisie dans les champs
+emailInput.addEventListener('input', checkFormValidity);
+passwordInput.addEventListener('input', checkFormValidity);
+
+// Écouteur d'événement pour gérer l'envoi du formulaire lors du clic sur le bouton
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    // Vérifier si les champs sont remplis avant d'appeler getAllCredentials()
+    const emailValue = emailInput.value.trim();
+    const passwordValue = passwordInput.value.trim();
+
+    if (emailValue !== '' && passwordValue !== '') {
+        getAllCredentials();
+    } else {
+        alert('Veuillez remplir tous les champs du formulaire.');
+    }
+});
